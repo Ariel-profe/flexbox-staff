6812101b383e4f6dc2e407517505b0e00dc56f65
Ariel Elias <eliasariel@live.com>
2022-04-12 17:57:20 -0300

    Primer comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { people } from './database';
+import { PeopleCard } from './components/PeopleCard';
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'> 
+      {
+        people.map( (person, index) => ((
+            <PeopleCard key={index} img={person.img} name={person.name} role={person.role} />
+          ))  )
+      }
     </div>
   );
 }
+
 
 export default App;
